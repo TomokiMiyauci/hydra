6f887277df09430c0d007ed17b8de388a1bbda7e
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2022-12-15 16:54:32 +0900

    feat(plugins): add custom attributes options to twind plugin

--- plugins/twind.ts
+++ plugins/twind.ts
@@ -1,13 +1,20 @@
 import {
-  inline,
+  extract,
   install,
   TwindConfig,
   TwindUserConfig,
 } from "https://esm.sh/v99/@twind/core@1.0.3";
+import { h, render } from "../deps.ts";
 import type { Plugin } from "../types.ts";
+
+export interface Options {
+  /** Customize inject style element attributes. */
+  readonly attrs: Record<string, string | boolean | number>;
+}
 
 export default function useTwind(
   config: TwindConfig | TwindUserConfig,
+  options?: Partial<Options>,
 ): Plugin {
   install(config as TwindConfig);
 
@@ -15,7 +22,21 @@ export default function useTwind(
     name: "twind",
 
     setup: (hydra) => {
-      hydra.onTransform("text/html", inline);
+      hydra.onTransform("text/html", (html) => inline(html, options?.attrs));
     },
   };
+}
+
+export function inline(
+  markup: string,
+  attrs?: Record<string, string | boolean | number>,
+): string {
+  const { html, css } = extract(markup);
+  const node = h(
+    "style",
+    { ...attrs, dangerouslySetInnerHTML: { __html: css } } as never,
+  );
+  const styleTag = render(node);
+
+  return html.replace("</head>", `${styleTag}</head>`);
 }
